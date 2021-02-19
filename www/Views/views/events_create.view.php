7c54cae9ae9a69349d66421e35fbec5dc1e35f02
Adel Senhadji <adelsenhadjii@gmail.com>
2021-02-19 23:31:05 +0100

    add elements to ui

--- www/Views/views/events_create.view.php
+++ www/Views/views/events_create.view.php
@@ -1,71 +1,76 @@
 <section class="card w-100 ">
-    <div>
+    <div class="container">
         <form action="#">
-            <div class="flex-column">
-                <h1>General informations</h1>
-                <label class="field">
-                    <span>Movie name</span>
-                    <input type="text">
-                </label>
-                <div class="flex">
-                    <div class="flex">
+            <div class="row">
+                <div class="col-9">
+                    <div class="flex-column">
+                        <h1>General informations</h1>
                         <label class="field">
-                            <span>Date</span>
-                            <input type="text" size="4" onInput="this.parentNode.dataset.value = this.value">
+                            <span>Movie name</span>
+                            <input type="text">
                         </label>
-                        <button class="button">
-                            <img src=" " alt=" ">
-                        </button>
+                        <div class="flex">
+                            <label class="field">
+                                <span>Date</span>
+                                <input type="date">
+                            </label>
+                            <label class="field">
+                                <span>Start</span>
+                                <input type="time">
+                            </label>
+                            <label class="field">
+                                <span>End</span>
+                                <input type="time">
+                            </label>
+                            <label class="field">
+                                <span>Room</span>
+                                <input type="text">
+                            </label>
+                        </div>
+                        <a href="#" class="link m-s">Add a session</a>
                     </div>
-                    <label class="field">
-                        <span>Start</span>
-                        <input type="number" size="4" onInput="this.parentNode.dataset.value = this.value">
-                    </label>
-                    <label class="field">
-                        <span>End</span>
-                        <input type="number" size="4" onInput="this.parentNode.dataset.value = this.value">
-                    </label>
-                    <label class="field">
-                        <span>Room</span>
-                        <input type="text" size="4" onInput="this.parentNode.dataset.value = this.value">
-                    </label>
+                    <div>
+                        <h1>Metadata</h1>
+                        <div class="flex">
+                            <label class="field">
+                                <span>Directed by</span>
+                                <input type="text">
+                            </label>
+                            <button class="button">Add</button>
+                        </div>
+                        <div class="flex">
+                            <label class="field">
+                                <span>Staring</span>
+                                <input type="text">
+                            </label>
+                            <button class="button">Add</button>
+                        </div>
+                        <div>
+                            <label class="field field-area">
+                                <span>Synopsis</span>
+                                <textarea rows="8"></textarea>
+                            </label>
+                        </div>
+                        <div class="flex">
+                            <label class="field">
+                                <span>Tags</span>
+                                <input type="text">
+                            </label>
+                            <button class="button">Add</button>
+                        </div>
+                    </div>
                 </div>
-                <a href="#" class="link m-s">Add a session</a>
-            </div>
-            <div>
-                <h1>Metadata</h1>
-                <div class="flex">
-                    <label class="field">
-                        <span>Directed by</span>
-                        <input type="text" size="4" onInput="this.parentNode.dataset.value = this.value">
-                    </label>
-                    <button class="button">Add</button>
-                </div>
-                <div class="flex">
-                    <label class="field">
-                        <span>Staring</span>
-                        <input type="text" size="4" onInput="this.parentNode.dataset.value = this.value">
-                    </label>
-                    <button class="button">Add</button>
-                </div>
-
-                <div>
-                    <label class="field">
-                        <span>Synopsis</span>
-                        <input type="text" size="4" onInput="this.parentNode.dataset.value = this.value">
-                    </label>
-                </div>
-
-
-                <div class="flex">
-                    <label class="field">
-                        <span>Tags</span>
-                        <input type="text" size="4" onInput="this.parentNode.dataset.value = this.value">
-                    </label>
-                    <button class="button">Add</button>
+                <div class="col-3 flex-column flex-bottom">
+                    <div class="flex-column flex-top">
+                        <h1>Movie Cover</h1>
+                        <!-- <img src="icons/image-placeholder.svg" alt="placeholder"> -->
+                        <div class="image-input-portrait">
+                            <button>+</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="flex flex-center">
+            <div class="container flex flex-center">
                 <button class="button button--success">Confirm</button>
                 <button class="button button--danger">Cancel</button>
             </div>
