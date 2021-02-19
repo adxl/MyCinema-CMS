690ef6710d16b9f6e8fddbfd5c13c8ec6860c60d
Fasso <fassory.diaby@gmail.com>
2021-02-20 00:02:13 +0100

    View gestion des utilisateurs

--- www/Views/views/users.view.php
+++ www/Views/views/users.view.php
@@ -1,3 +1,129 @@
 <div class="card w-100 ">
-    users
+    <input type="text" id="rechercheUsers" name="rechercheUsers" placeholder="Recherche utilisateurs">
+</div>
+
+<div class="card w-100 my-s">
+    <h1>Tableau des utilisateurs</h1>
+    <table id="tabUsers">
+        <thead>
+            <tr>
+                <td>#</td>
+                <td>Nom</td>
+                <td>Prénom</td>
+                <td>Informations</td>
+                <td>Informations</td>
+                <td>Informations</td>
+                <td>Action</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>
+                    <a href="#"><img src="../dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>2</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>
+                    <a href="#"><img src="../dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>3</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>
+                    <a href="#"><img src="../dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>4</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>
+                    <a href="#"><img src="../dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>5</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>
+                    <a href="#"><img src="../dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>6</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>
+                    <a href="#"><img src="../dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>7</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>
+                    <a href="#"><img src="../dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>8</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>
+                    <a href="#"><img src="../dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>9</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>Données</td>
+                <td>
+                    <a href="#"><img src="../dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </div>
