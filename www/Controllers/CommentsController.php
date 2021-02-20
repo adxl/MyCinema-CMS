a8622b1a37acd846b3172187295e2edac93e40b9
Nocktiss <anthony.grupposo@gmail.com>
2021-02-20 12:34:51 +0100

    [update] new controller and view for comments page

---
+++ www/Controllers/CommentsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\View;
+
+class CommentsController
+{
+
+    public function showCommentsAction()
+    {
+        $view = new View("comments");
+        $view->assign("title", 'Comment management');
+    }
+}
