2d3b93ce43bac2ed3c057cd6bef0cf2f73fb36cd
Adel Senhadji <adelsenhadjii@gmail.com>
2021-02-23 16:45:56 +0100

    fix users table

--- www/Views/views/users.view.php
+++ www/Views/views/users.view.php
@@ -1,32 +1,29 @@
 <div class="flex my-s mt-0">
-    <img src="/Views/dist/icons/black-calendar.svg" alt="icon">
+    <img src="/Views/dist/icons/black-user.svg" alt="icon">
     <h1 class="mx-s"><?= $title; ?></h1>
 </div>
 
-<div class=" w-100 flex flex-right">
+<div class=" w-100 flex flex-right my-m">
     <div class="searchbar">
         <img src="/Views/dist/icons/gray-zoom.svg" alt="">
-        <input type="text" name="rechercheUsers" placeholder="Recherche utilisateurs">
+        <input type="text" name="user-search" placeholder="Search an user">
     </div>
     <button class="button">Rechercher</button>
 </div>
 
-<div class="card w-100 my-s">
-    <h1>Tableau des utilisateurs</h1>
-    <table id="tabUsers">
+<div>
+    <table class="table card">
         <thead>
             <tr>
-                <th>#</th>
                 <th>Nom</th>
                 <th>Prénom</th>
                 <th>Email</th>
                 <th>Permissions</th>
-                <th>Action</th>
+                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td>1</td>
                 <td>Diaby</td>
                 <td>Nfassory</td>
                 <td>nfassory.diaby@esgi.fr</td>
@@ -35,9 +32,7 @@
                     <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
                 </td>
             </tr>
-
             <tr>
-                <td>2</td>
                 <td>Dechelette</td>
                 <td>François</td>
                 <td>francois.dechelette@esgi.fr</td>
@@ -46,9 +41,106 @@
                     <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
                 </td>
             </tr>
-
             <tr>
-                <td>3</td>
+                <td>Grupposo</td>
+                <td>Anthony</td>
+                <td>anthony.grupposo@esgi.fr</td>
+                <td>Administrateur</td>
+                <td>
+                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+            <tr>
+                <td>Senhadji</td>
+                <td>Adel</td>
+                <td>adel.senhadji@esgi.fr</td>
+                <td>Administrateur</td>
+                <td>
+                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+            <tr>
+                <td>Marchand</td>
+                <td>Maxime</td>
+                <td>maxime.marchand@esgi.fr</td>
+                <td>Administrateur</td>
+                <td>
+                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+            <tr>
+                <td>Sombie</td>
+                <td>Edouard</td>
+                <td>edouard.sombie@esgi.fr</td>
+                <td>Gestionnaire</td>
+                <td>
+                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+            <tr>
+                <td>Serval</td>
+                <td>Jérémy</td>
+                <td>jeremy.serval@esgi.fr</td>
+                <td>Gestionnaire</td>
+                <td>
+                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+            <tr>
+                <td>Skrzypczyk</td>
+                <td>Yves</td>
+                <td>yves.skrzypczyk@esgi.fr</td>
+                <td>Client</td>
+                <td>
+                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+            <tr>
+                <td>Coat</td>
+                <td>Gael</td>
+                <td>gael.coat@esgi.fr</td>
+                <td>Client</td>
+                <td>
+                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+            <tr>
+                <td>Senhadji</td>
+                <td>Adel</td>
+                <td>adel.senhadji@esgi.fr</td>
+                <td>Administrateur</td>
+                <td>
+                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+            <tr>
+                <td>Marchand</td>
+                <td>Maxime</td>
+                <td>maxime.marchand@esgi.fr</td>
+                <td>Administrateur</td>
+                <td>
+                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+            <tr>
+                <td>Diaby</td>
+                <td>Nfassory</td>
+                <td>nfassory.diaby@esgi.fr</td>
+                <td>Administrateur</td>
+                <td>
+                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+            <tr>
+                <td>Dechelette</td>
+                <td>François</td>
+                <td>francois.dechelette@esgi.fr</td>
+                <td>Administrateur</td>
+                <td>
+                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
+                </td>
+            </tr>
+            <tr>
                 <td>Grupposo</td>
                 <td>Anthony</td>
                 <td>anthony.grupposo@esgi.fr</td>
@@ -58,71 +150,6 @@
                 </td>
             </tr>
 
-            <tr>
-                <td>4</td>
-                <td>Senhadji</td>
-                <td>Mohammed Adel</td>
-                <td>m_adel.senhadji@esgi.fr</td>
-                <td>Administrateur</td>
-                <td>
-                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
-                </td>
-            </tr>
-
-            <tr>
-                <td>5</td>
-                <td>Marchand</td>
-                <td>Maxime</td>
-                <td>maxime.marchand@esgi.fr</td>
-                <td>Administrateur</td>
-                <td>
-                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
-                </td>
-            </tr>
-
-            <tr>
-                <td>6</td>
-                <td>Sombie</td>
-                <td>Edouard</td>
-                <td>edouard.sombie@esgi.fr</td>
-                <td>Gestionnaire</td>
-                <td>
-                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
-                </td>
-            </tr>
-
-            <tr>
-                <td>7</td>
-                <td>Serval</td>
-                <td>Jérémy</td>
-                <td>jeremy.serval@esgi.fr</td>
-                <td>Gestionnaire</td>
-                <td>
-                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
-                </td>
-            </tr>
-
-            <tr>
-                <td>8</td>
-                <td>Skrzypczyk</td>
-                <td>Yves</td>
-                <td>yves.skrzypczyk@esgi.fr</td>
-                <td>Client</td>
-                <td>
-                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
-                </td>
-            </tr>
-
-            <tr>
-                <td>9</td>
-                <td>Coat</td>
-                <td>Gael</td>
-                <td>gael.coat@esgi.fr</td>
-                <td>Client</td>
-                <td>
-                    <a href="#"><img src="/Views/dist/icons/blue-eye.svg"></a>
-                </td>
-            </tr>
         </tbody>
     </table>
 </div>
