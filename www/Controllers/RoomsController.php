534f38cdf58d9d97ab9188f33058e8b08f890baf
Adel Senhadji <adelsenhadjii@gmail.com>
2021-04-08 17:51:14 +0200

    feat : create room

--- www/Controllers/RoomsController.php
+++ www/Controllers/RoomsController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controllers;
 
+use App\Core\Database;
 use App\Core\View;
+
+// use App\Core\FormValidator;
+use App\Models\Room as RoomModel;
+// use App\Models\Page;
 
 class RoomsController
 {
@@ -11,5 +16,15 @@ class RoomsController
     {
         $view = new View("rooms");
         $view->assign("title", 'Rooms Management');
+    }
+
+    public function createRoom()
+    {
+        $room = new RoomModel();
+
+        $room->setLabel("La Cigale");
+        $room->setCapacity(1000);
+
+        $room->save();
     }
 }
--- www/Core/Database.php
+++ www/Core/Database.php
@@ -15,8 +15,8 @@ class Database
 			$this->pdo = new \PDO(DBDRIVER . ":host=" . DBHOST . ";dbname=" . DBNAME . ";port=" . DBPORT, DBUSER, DBPWD);
 			$classExploded = explode("\\", get_called_class());
 			$this->table = strtolower(DBPREFIXE . end($classExploded));
-			$query = $this->pdo->prepare("SELECT * FROM faman_user");
-			$query->execute();
+
+			echo $this->table;
 		} catch (Exception $e) {
 			die("Erreur SQL : " . $e->getMessage());
 		}
@@ -36,12 +36,16 @@ class Database
 			$query = "INSERT INTO " . $this->table . "(" . implode(',', array_keys($column)) . ") 
 			VALUES (:" . implode(',:', array_keys($column)) . ") ";
 
-
 			$stmt = $this->pdo->prepare($query); //1 
-
 
 		} else {
 		}
+
+		echo "<pre>";
+		print_r($column);
+		echo "</pre>";
+
+		echo $query . PHP_EOL;
 
 		$stmt->execute($column);
 	}
