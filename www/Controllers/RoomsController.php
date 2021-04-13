59cc855b1979ce98439f8b12bb3846db3c556f79
Adel Senhadji <adelsenhadjii@gmail.com>
2021-04-13 20:48:23 +0200

    Integration rooms

--- www/Controllers/RoomsController.php
+++ www/Controllers/RoomsController.php
@@ -36,6 +36,10 @@ class RoomsController
         $rooms = $roomModel->findAll();
 
         $view->assign("rooms", $rooms);
+
+        //TODO: faut check si user est admin ou pas 
+        $isAdmin = true;
+        $view->assign("isAdmin", $isAdmin);
     }
 
     public function showOneRoomAction($id)
@@ -50,13 +54,36 @@ class RoomsController
         $view->assign("room", $room);
     }
 
+    public function showCreateRoomAction()
+    {
+        $view = new View("rooms_create");
+        $view->assign("title", 'Rooms Management > Create Room');
+    }
+
+
+    public function showEditRoomAction()
+    {
+        $id = Helpers::getQueryParam('id');
+        if ($id) {
+            $roomModel = new RoomModel();
+            $room = $roomModel->findById($id);
+
+            if ($room) {
+                $view = new View("rooms_edit");
+                $view->assign("title", 'Rooms Management > Edit Room');
+                return;
+            }
+        }
+        $view = new View('404');
+    }
+
     public function createRoom()
     {
-        $room = new RoomModel();
+        // $room = new RoomModel();
 
-        $room->setLabel("Bercy");
-        $room->setCapacity(3000);
+        // $room->setLabel("Bercy");
+        // $room->setCapacity(3000);
 
-        $room->save();
+        // $room->save();
     }
 }
