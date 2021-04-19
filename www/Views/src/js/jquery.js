bc670a3c91f8bbd3b1f522a06d25d0b0d05dbb43
Adel Senhadji <adelsenhadjii@gmail.com>
2021-04-19 23:15:54 +0200

    Fix : remove babel mix

--- www/Views/src/js/jquery.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-
-import '/Views/node_modules/jquery/dist/jquery.min.js';
-
-export default $;
--- www/Views/webpack.mix.js
+++ www/Views/webpack.mix.js
@@ -1,13 +1,8 @@
 const mix = require('laravel-mix');
-const del = require('del');
 
 mix.setPublicPath('./dist');
 
-mix.babel('./src/js/main.js', './dist/main.js');
-
-mix.minify('./dist/main.js').then(() => {
-	del('./dist/main.js');
-});
+mix.js('./src/js/main.js', './dist/main.min.js');
 
 mix.sass('./src/scss/back.scss', './dist/back.css');
 mix.sass('./src/scss/front.scss', './dist/front.css');
