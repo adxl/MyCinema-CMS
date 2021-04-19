2d9cf496777bfd66b076106bc0057923885aab1b
Adel Senhadji <adelsenhadjii@gmail.com>
2021-04-20 00:03:09 +0200

    Feat : add media query mixins

--- www/Views/templates/back.tpl.php
+++ www/Views/templates/back.tpl.php
@@ -5,6 +5,7 @@
 	<!-- <base href="/Views/dist/"> -->
 	<meta charset="UTF-8">
 	<meta name="description" content="description de la page de back">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Template de back</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
 	<link rel="stylesheet" href="/Views/dist/back.css">
