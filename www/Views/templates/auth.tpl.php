ff7e28dfe5c9fc04adbac0151447ef55f22826a7
Fasso <fassory.diaby@gmail.com>
2021-04-28 14:41:53 +0200

    integration auth

--- www/Views/templates/auth.tpl.php
+++ www/Views/templates/auth.tpl.php
@@ -5,6 +5,7 @@
     <base href="Views/dist/">
     <meta charset="UTF-8">
     <meta name="description" content="description de la page d'authentification'">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Template d'authentification</title>
     <link rel="stylesheet" href="/Views/dist/auth.css">
     <script src="main.js"></script>
@@ -17,14 +18,16 @@
     <div class="h-50">
         <img class="w-100 h-100 cover" src="https://jeromeobiols.com/wordpress/wp-content/uploads/30122016_JOB5054F_Lever_Brenva-1920.jpg">
     </div>
-    <div class="container bg-white h-50">
-        <div class="container h-40">
+    <div class="w-100 bg-white h-50 flex-column flex-between">
+        <div class="flex-column b">
             <label>Email</label>
+            <input class="login" type="text">
         </div>
-        <div class="container h-40">
+        <div class="flex-column b mb-auto">
             <label>Password</label>
+            <input class="login" type="text">
         </div>
-        <div class="container h-20">
+        <div class="b">
             <label>Other</label>
         </div>
     </div>
