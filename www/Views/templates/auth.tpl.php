5c1dcb8ace517502ed4780c35faafe3d7e564d78
nfassorydiaby <68150614+nfassorydiaby@users.noreply.github.com>
2021-05-08 17:58:43 +0200

    Integration: Login #25
    * first pull integration/login-register
    * template auth
    * integration auth
    * integration
    * Fix auth integration
    
    Co-authored-by: Adel Senhadji <adelsenhadjii@gmail.com>

---
+++ www/Views/templates/auth.tpl.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="description de la page d'authentification'">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Template d'authentification</title>
+    <link rel="stylesheet" href="/Views/dist/front.css">
+    <script type="module" src="/Views/dist/main.min.js"></script>
+</head>
+
+<body>
+    <section class="h-100">
+        <?php include $this->view ?>
+    </section>
+</body>
+
+</html>
---
+++ www/Views/views/f_login.view.php
@@ -0,0 +1,18 @@
+<div class="w-100 bg-white flex-column p-l">
+    <h1 class="text-center mt-m mb-l">Login</h1>
+    <div class="flex-column mb-m">
+        <label>Email</label>
+        <input class="input" type="text">
+    </div>
+    <div class="flex-column mb-xl">
+        <div class="mb-s flex-column">
+            <label>Password</label>
+            <input class="input" type="text">
+        </div>
+        <a href="#" class="link">Mot de passe oublié ?</a>
+    </div>
+
+    <div class="flex flex-center">
+        <button class="button button--success w-50">Login</button>
+    </div>
+</div>
