dae8b64d1160791bb6d80c9da9a42a18cca023c1
Maxime MARCHAND <mmarchand6@myges.fr>
2021-05-27 20:20:11 +0200

    Add error 500 route

---
+++ www/Views/views/f_500.view.php
@@ -0,0 +1,5 @@
+<div class="flex-column flex-center flex-middle" style="margin-top: 200px;">
+    <h1 style="font-size: 10em;">500</h1>
+    <p class="mb-xl">Une erreur est survenue, veuillez réessayer ultérieurement.</p>
+    <a href="/" class="button button--danger"> Retour à l'accueil </a>
+</div>
