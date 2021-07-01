8061393ab13a495ebb9b4b4da0ed8e0af189bfa2
Adel Senhadji <adelsenhadjii@gmail.com>
2021-07-02 01:14:50 +0200

    Fix : handle null data

--- www/Views/views/f_events.view.php
+++ www/Views/views/f_events.view.php
@@ -1,55 +1,59 @@
 <!-- PROCHAIN EVENT -->
-<section class="bg-dark text-white">
-    <h1 class="text-center text-white mt-l mb-m">À L'AFFICHE</h1>
-    <div class="p-l">
-        <img id="events-main-cover" class="w-100 cover rounded" src="https://images.affiches-et-posters.com//albums/3/4535/poster-film-inception-2350.jpg" alt="poster">
-    </div>
-    <div class="flex">
-        <div class="flex flex-center rounded col-6">
-            <div class="p-l">
-                <img class="h-100 w-100 cover rounded" src="https://images.affiches-et-posters.com//albums/3/4535/poster-film-inception-2350.jpg" alt="poster">
-            </div>
+<?php if ($nextEvent) : ?>
+    <section class="bg-dark text-white">
+        <h1 class="text-center text-white mt-l mb-m">À L'AFFICHE</h1>
+        <div class="p-l">
+            <img id="events-main-cover" class="w-100 cover rounded" src="https://images.affiches-et-posters.com//albums/3/4535/poster-film-inception-2350.jpg" alt="poster">
         </div>
-        <div class="flex-column p-l col-6">
-            <h1 class="text-white mb-m"><?= $nextEvent['title']; ?></h1>
-            <p class="mb-s text-underline">Synopsis : </p>
-            <p class="mb-l"><?= $nextEvent['synopsis']; ?></p>
-            <div class="flex">
-                <p class="mb-s text-underline">Par </p>
-                <p> : <?= $nextEvent['directors']; ?></p>
+        <div class="flex">
+            <div class="flex flex-center rounded col-6">
+                <div class="p-l">
+                    <img class="h-100 w-100 cover rounded" src="https://images.affiches-et-posters.com//albums/3/4535/poster-film-inception-2350.jpg" alt="poster">
+                </div>
             </div>
-            <div class="flex">
-                <p class="text-underline">Avec </p>
-                <p> : <?= $nextEvent['actors']; ?></p>
-            </div>
-            <div class="flex mt-auto mb-m">
-                <?php foreach ($nextEvent['tags'] as $tag) : ?>
-                    <p class="bg-dark-gray p-s mr-s rounded"> <?= mb_strtoupper($tag); ?> </p>
-                <?php endforeach; ?>
-            </div>
-        </div>
-    </div>
-</section>
-
-
-<!-- EVENTS A VENIR -->
-<section id="incoming-events-section">
-    <h1 class="mt-l mb-m">Prochainement : </h1>
-    <div class="row">
-        <?php foreach ($incomingEvents as $event) : ?>
-            <div class="flex-column col-3">
-                <img class="w-100 rounded" src="https://ae01.alicdn.com/kf/HTB1RHpBIXXXXXbLXFXXq6xXFXXXz/Free-shipping-HD-wallpaper-poster-Captain-America-Civil-War-movie-poster-Smith-24x36inch.jpg_Q90.jpg_.webp" alt="poster">
-                <p><?= $event['title']; ?></p>
-                <p><?= $event['directors']; ?></p>
-                <div class="flex mt-auto">
-                    <?php foreach ($event['tags'] as $tag) : ?>
-                        <p class="bg-lighter p-s mr-s rounded"> <?= mb_strtoupper($tag); ?> </p>
+            <div class="flex-column p-l col-6">
+                <h1 class="text-white mb-m"><?= $nextEvent['title']; ?></h1>
+                <p class="mb-s text-underline">Synopsis : </p>
+                <p class="mb-l"><?= $nextEvent['synopsis']; ?></p>
+                <div class="flex">
+                    <p class="mb-s text-underline">Par </p>
+                    <p> : <?= $nextEvent['directors']; ?></p>
+                </div>
+                <div class="flex">
+                    <p class="text-underline">Avec </p>
+                    <p> : <?= $nextEvent['actors']; ?></p>
+                </div>
+                <div class="flex mt-auto mb-m">
+                    <?php foreach ($nextEvent['tags'] as $tag) : ?>
+                        <p class="bg-dark-gray p-s mr-s rounded"> <?= mb_strtoupper($tag); ?> </p>
                     <?php endforeach; ?>
                 </div>
             </div>
-        <?php endforeach ?>
-    </div>
-</section>
+        </div>
+    </section>
+<?php endif ?>
+
+
+<!-- EVENTS A VENIR -->
+<?php if (!empty($incomingEvents)) : ?>
+    <section id="incoming-events-section">
+        <h1 class="mt-l mb-m">Prochainement : </h1>
+        <div class="row">
+            <?php foreach ($incomingEvents as $event) : ?>
+                <div class="flex-column col-3">
+                    <img class="w-100 rounded" src="https://ae01.alicdn.com/kf/HTB1RHpBIXXXXXbLXFXXq6xXFXXXz/Free-shipping-HD-wallpaper-poster-Captain-America-Civil-War-movie-poster-Smith-24x36inch.jpg_Q90.jpg_.webp" alt="poster">
+                    <p><?= $event['title']; ?></p>
+                    <p><?= $event['directors']; ?></p>
+                    <div class="flex mt-auto">
+                        <?php foreach ($event['tags'] as $tag) : ?>
+                            <p class="bg-lighter p-s mr-s rounded"> <?= mb_strtoupper($tag); ?> </p>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            <?php endforeach ?>
+        </div>
+    </section>
+<?php endif ?>
 
 
 <!-- TOUS LES EVENTS -->
@@ -58,7 +62,7 @@
     <div class="row">
         <?php foreach ($events as $event) : ?>
             <div class="flex-column col-3">
-                <a href="/event?id=<?= $event['id']; ?>" target="_blank">
+                <a href="/events?id=<?= $event['id']; ?>">
                     <img class="w-100 rounded" src="https://ae01.alicdn.com/kf/HTB1RHpBIXXXXXbLXFXXq6xXFXXXz/Free-shipping-HD-wallpaper-poster-Captain-America-Civil-War-movie-poster-Smith-24x36inch.jpg_Q90.jpg_.webp" alt="poster">
                 </a>
                 <p><?= $event['title']; ?></p>
