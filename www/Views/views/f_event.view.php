f92e1c28ad423596fe0f0fb48e7e817d59063099
Fasso <fassory.diaby@gmail.com>
2021-07-02 00:20:18 +0200

    add page event detail

--- www/Views/views/f_event.view.php
+++ www/Views/views/f_event.view.php
@@ -1,18 +1,66 @@
-<section>
+<section id="next-event-section">
+    <h1 class="text-center"><?= $event['title']; ?></h1>
+
     <div class="row">
-        <h1><?= $event['title']; ?></h1>
+        <div class="flex flex-center rounded col-6 h-100">
+            <div class="h-100 p-l">
+                <img class="w-100 rounded" src="https://ae01.alicdn.com/kf/HTB1RHpBIXXXXXbLXFXXq6xXFXXXz/Free-shipping-HD-wallpaper-poster-Captain-America-Civil-War-movie-poster-Smith-24x36inch.jpg_Q90.jpg_.webp" alt="poster">
+            </div>
+        </div>
+        <div class="flex-column p-m col-6">
+            <p>Synopsis : </p>
+            <p class="mb-l"><?= $event['synopsis']; ?></p>
+
+            <p class="mb-s">Par : <?= $event['directors']; ?></p>
+            <p>Avec : <?= $event['actors']; ?></p>
+            <div class="flex mt-auto mb-m">
+                <?php foreach ($event['tags'] as $tag) : ?>
+                    <p class="bg-lighter p-s mr-s rounded"> <?= mb_strtoupper($tag); ?> </p>
+                <?php endforeach; ?>
+            </div>
+        </div>
     </div>
 
 
+
+
+</section>
+
+<section class="mt-l">
     <!-- input ajout commentaire -->
-    <div class="row">
-        <?php App\Core\FormBuilder::render($commentForm); ?>
+    <div class="row card w-100">
+        <h1 class="text-center">Ajouter un commentaire : </h1>
+        <div class="flex flex-center w-100">
+            <?php App\Core\FormBuilder::render($commentForm); ?>
+        </div>
     </div>
+</section>
 
-
-
+<section class="mt-l mb-l">
     <!-- Liste des commentaires -->
     <div>
-        <pre> <?php print_r($comments) ?> </pre>
+        <table class="event-details-comments-table card">
+            <thead>
+                <tr>
+                    <th>Auteur</th>
+                    <th>Commentaire</th>
+                    <th>Date</th>
+                </tr>
+
+            </thead>
+            <tbody>
+                <?php foreach ($comments as $comment) : ?>
+                    <tr>
+                        <td><?= $comment['name']; ?></td>
+                        <td><?= $comment['content']; ?></td>
+                        <td><?= $comment['date']; ?></td>
+                    </tr>
+                <?php endforeach ?>
+
+            </tbody>
+        </table>
+        <!--        <pre> --><?php //$comments ? print_r($comments) : null
+                                ?>
+        <!-- </pre>-->
     </div>
 </section>
